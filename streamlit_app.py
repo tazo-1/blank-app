5d34e66a0be1a981b30e558436499fef8f3e030e
tazo-1 <attayafikri491@gmail.com>
2025-05-22 09:43:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Attaya fikri mantap")
+st.write("popopop")
+st.image*
